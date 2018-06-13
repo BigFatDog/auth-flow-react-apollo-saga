ef9b6cc576ff667b5819b100684670eb8029c86b
Yun Xing <ink.xing.yun@gmail.com>
2018-06-13 14:50:04 -0400

    upgrade core injectors

--- app/utils/reducerInjectors.js
+++ app/utils/reducerInjectors.js
@@ -12,7 +12,7 @@ export function injectReducerFactory(store, isValid) {
 
     invariant(
       isString(key) && !isEmpty(key) && isFunction(reducer),
-      '(client/utils...) injectReducer: Expected `reducer` to be a reducer function'
+      '(core/runtime...) injectReducer: Expected `reducer` to be a reducer function'
     );
 
     // Check `store.injectedReducers[key] === reducer` for hot reloading when a key is the same but a reducer is different
