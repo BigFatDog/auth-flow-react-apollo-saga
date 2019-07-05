3d56ae5a77532bb9454c4e6368f720a29f5bf34c
Yun Xing <ink.xing.yun@gmail.com>
2019-07-04 21:48:48 -0400

    significantly reduce post code

--- app/containers/PostList/index.js
+++ app/containers/PostList/index.js
@@ -1,34 +1,15 @@
-import React, { useEffect, memo } from 'react';
+import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
-import { connect } from 'react-redux';
 import { compose } from 'redux';
-import { createStructuredSelector } from 'reselect';
 import { graphql } from 'react-apollo';
 
-import { useInjectReducer } from '../../core/runtime/injectReducer';
-import { useInjectSaga } from '../../core/runtime/injectSaga';
 import messages from './messages';
 
-import { changeUsername } from './actions';
-import { makeSelectUsername } from './selectors';
-import reducer from './reducer';
-import saga from './saga';
-
 import DashboardListQuery from './Posts.graphql';
-const key = 'home';
 
 const HomePage = props => {
-  useInjectReducer({ key, reducer });
-  useInjectSaga({ key, saga });
-
-  const { username, posts, onSubmitForm } = props;
-
-  useEffect(() => {
-    if (username) {
-      onSubmitForm();
-    }
-  }, []);
+  const { posts } = props;
 
   const aList = posts || [];
   const postList = aList.map(d => (
@@ -51,28 +32,7 @@ HomePage.propTypes = {
   error: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
   repos: PropTypes.oneOfType([PropTypes.array, PropTypes.bool]),
   onSubmitForm: PropTypes.func,
-  username: PropTypes.string,
-  onChangeUsername: PropTypes.func,
 };
-
-const mapDispatchToProps = dispatch => {
-  return {
-    onChangeUsername: evt => dispatch(changeUsername(evt.target.value)),
-    onSubmitForm: evt => {
-      if (evt !== undefined && evt.preventDefault) evt.preventDefault();
-      dispatch(loadRepos());
-    },
-  };
-};
-
-const mapStateToProps = createStructuredSelector({
-  username: makeSelectUsername(),
-});
-
-const withConnect = connect(
-  mapStateToProps,
-  mapDispatchToProps
-);
 
 const withGraphQL = graphql(DashboardListQuery, {
   props: ({ data }) => {
@@ -84,6 +44,5 @@ const withGraphQL = graphql(DashboardListQuery, {
 
 export default compose(
   withGraphQL,
-  withConnect,
   memo
 )(HomePage);
